6c32f7f1314d40df1b9f0689aa1c35ad0118b243
GhzGarage <qbcore@programmer.net>
2021-04-10 21:53:05 -0500

    Initial Commit

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'QB-FitBit'
+version '1.0.0'
+
+ui_page "html/index.html"
+
+server_scripts {
+	"server/main.lua"
+}
+
+client_scripts {
+	"client/main.lua"
+}
+
+files {
+	"html/*"
+}
