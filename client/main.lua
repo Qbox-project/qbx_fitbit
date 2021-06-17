1cb014da8894cf1763b4c7b2980bfce955f5a56a
JericoFX <56100605+JericoFX@users.noreply.github.com>
2021-06-17 02:28:50 -0300

    Prevent infinite Loop
    the while true is running every time.

--- client/main.lua
+++ client/main.lua
@@ -12,12 +12,11 @@ Keys = {
 QBCore = nil
 
 Citizen.CreateThread(function()
-    while true do
-        Citizen.Wait(10)
-        if QBCore == nil then
-            TriggerEvent('QBCore:GetObject', function(obj) QBCore = obj end)
-            Citizen.Wait(200)
-        end
+    while not QBCore do
+        TriggerEvent('QBCore:GetObject', function(obj)
+            QBCore = obj
+        end)
+      Wait(200)
     end
 end)
 
