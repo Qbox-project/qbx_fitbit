d53836c40c26efa3c1d2b2123f4a71c0988a454a
ev.vanillaa <80897991+evanillaa@users.noreply.github.com>
2021-09-22 22:50:06 +0300

    add //

--- html/app.js
+++ html/app.js
@@ -3,7 +3,7 @@ var openedApp = ".main-screen"
 qbFitbit = {}
 
 $(document).ready(function(){
-    console.log('Fitbit is loaded..')
+    // console.log('Fitbit is loaded..')
 
     window.addEventListener('message', function(event){
         var eventData = event.data;
