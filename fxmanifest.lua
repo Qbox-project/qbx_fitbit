911ab3109821552f1fafc97be5b531a477d0fdb3
buddizer <iw3io8ge@duck.com>
2022-09-13 22:30:03 +0000

    add auto switch locale system

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,13 +7,14 @@ version '1.0.0'
 ui_page 'html/index.html'
 
 shared_scripts {
-	'@qb-core/shared/locale.lua',
-	'locales/en.lua'
+    '@qb-core/shared/locale.lua',
+    'locales/en.lua',
+    'locales/*.lua'
 }
 
 server_script 'server/main.lua'
 client_script 'client/main.lua'
 
 files {
-	'html/*'
+    'html/*'
 }
