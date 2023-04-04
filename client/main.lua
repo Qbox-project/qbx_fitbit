d78dd40c3c0db47988068bec030a6d6dd4dba902
ChatDisabled <44729807+ChatDisabled@users.noreply.github.com>
2023-04-04 22:54:02 +0200

    fix: export naming

--- client/main.lua
+++ client/main.lua
@@ -1,4 +1,4 @@
-local QBCore = exports['qb-core']:GetCoreObject()
+local QBCore = exports['qbx-core']:GetCoreObject()
 local PlayerData = QBCore.Functions.GetPlayerData()
 local isLoggedIn = LocalPlayer.state.isLoggedIn
 local hasFitbit = false
--- server/main.lua
+++ server/main.lua
@@ -1,4 +1,4 @@
-local QBCore = exports['qb-core']:GetCoreObject()
+local QBCore = exports['qbx-core']:GetCoreObject()
 
 QBCore.Functions.CreateUseableItem("fitbit", function(source)
     TriggerClientEvent('qb-fitbit:use', source)
